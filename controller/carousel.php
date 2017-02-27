9831ac1aefd237234470353b3440615321b0fca9
T731\saras <sarassoroberto@gmail.com>
2017-02-27 13:08:01 +0100

    Inizio creazione dello slider full screen

---
+++ controller/carousel.php
@@ -0,0 +1,19 @@
+<?php
+include_once './class/Page.php';
+include_once './class/Cartolina.php';
+include_once './model/CartolinaModel.php';
+
+//page e una view
+$page = new Page($controller);
+$model = new CartolinaModel();
+
+$model->setFolder("dummy-images/upload");
+$data = $model->readAll();
+
+$page->setSection("Carousel full screen");
+$page->setBrand("Cartolina");
+$page->setTitle("carousel full screen");
+$page->setData('immagini', $data);
+$page->setContent('view/imageList.php');
+
+$page->render();
